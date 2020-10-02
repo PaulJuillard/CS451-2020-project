7e67bf26d1e9ef554f92f78032b073b81838991a
PaulJuillard <yt.pjuillard@gmail.com>
2020-10-02 16:43:54 +0200

    first packets

--- template_java/src/main/java/cs451/Main.java
+++ template_java/src/main/java/cs451/Main.java
@@ -3,7 +3,10 @@ package cs451;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class Main {
 
@@ -37,8 +40,14 @@ public class Main {
 
         System.out.println("My id is " + parser.myId() + ".");
         System.out.println("List of hosts is:");
+
+        int myPort;
+
         for (Host host: parser.hosts()) {
             System.out.println(host.getId() + ", " + host.getIp() + ", " + host.getPort());
+            if(host.getId() == parser.myId()){
+                myPort = host.getPort();
+            }
         }
 
         System.out.println("Barrier: " + parser.barrierIp() + ":" + parser.barrierPort());
@@ -49,5 +58,30 @@ public class Main {
         }
 
         BarrierParser.Barrier.waitOnBarrier();
+
+        // send 
+
+        DatagramSocket socket = new DatagramSocket(myPort);
+
+        String message = "Hi i'm host number" + pid;
+
+        for(Host host : parser.hosts()){
+            byte[] s_buf = new byte[256];
+
+            InetAddress address = host.getIp();
+            s_buf = message.getBytes();
+            DatagramPacket packet = new DatagramPacket(s_buf, s_buf.length, address, host.getPort());
+            socket.send(packet);
+        }
+        
+        byte[] r_buf = new byte[256];
+        DatagramPacket r_p = new DatagramPacket(r_buf, r_buf.length);
+
+        socket.receive(r_p);
+        String received = new String(r_buf, StandardCharsets.UTF_8);
+
+        System.out.println(received);
+
+
     }
 }
